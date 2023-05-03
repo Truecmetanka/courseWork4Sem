701310a20865fce80b24db3e723a086ff1120936
Danil <matrosovdan@mail.ru>
2023-05-03 15:40:50 +0300

    init commit

---
+++ flightSearchSystem/src/main/java/ru/coursework/flightSearchSystem/FlightSearchSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.coursework.flightSearchSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchSystemApplication.class, args);
+	}
+
+}
